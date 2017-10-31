8da3031b303c81fb2d74c22b9303d22bd052182b
whisper2shade <1337036+whisper2shade@users.noreply.github.com>
2017-10-31 08:25:19 +0100

    SteamIdChecking

--- src/main/java/org/nyxcode/wurm/timedpay/TimedPay.java
+++ src/main/java/org/nyxcode/wurm/timedpay/TimedPay.java
@@ -1,20 +1,18 @@
 package org.nyxcode.wurm.timedpay;
 
-        import com.wurmonline.server.Players;
-        import com.wurmonline.server.WurmCalendar;
-        import com.wurmonline.server.players.Player;
-        import org.gotti.wurmunlimited.modloader.classhooks.HookManager;
-        import org.gotti.wurmunlimited.modloader.interfaces.Configurable;
-        import org.gotti.wurmunlimited.modloader.interfaces.Initable;
-        import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
-        import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
+import com.wurmonline.server.Players;
+import com.wurmonline.server.WurmCalendar;
+import com.wurmonline.server.players.Player;
+import org.gotti.wurmunlimited.modloader.classhooks.HookManager;
+import org.gotti.wurmunlimited.modloader.interfaces.Configurable;
+import org.gotti.wurmunlimited.modloader.interfaces.Initable;
+import org.gotti.wurmunlimited.modloader.interfaces.PreInitable;
+import org.gotti.wurmunlimited.modloader.interfaces.WurmServerMod;
 
-        import java.io.IOException;
-        import java.util.Arrays;
-        import java.util.List;
-        import java.util.Properties;
-        import java.util.logging.Level;
-        import java.util.logging.Logger;
+import java.io.IOException;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by whisper2shade on 22.01.2017.
@@ -49,13 +47,18 @@ public class TimedPay implements WurmServerMod, Initable, PreInitable, Configura
     }
 
     public long addMoneyToLoggedPlayers(Players players) {
+        Set<String> processedSteamIds = new HashSet<>();
         long currentTime = WurmCalendar.getCurrentTime();
         if (currentTime > lastPayout + payoutInterval) {
             logger.log(Level.FINE, "executing payout");
             lastPayout = currentTime;
             List<Player> playerList = Arrays.asList(players.getPlayers());
             for (Player player : playerList) {
-                addMoney(player);
+                String steamId = player.SteamId;
+                if (!processedSteamIds.contains(steamId)) {
+                    addMoney(player);
+                    processedSteamIds.add(steamId);
+                }
             }
         }
         return currentTime;
